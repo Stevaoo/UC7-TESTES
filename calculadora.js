aab8e4ec107851d755e1a0c2a29d3a21b49e2767
Stevão Rodrigues <162615972+Stevaoo@users.noreply.github.com>
2024-12-19 20:45:48 -0300

    Update calculadora.js

--- calculadora.js
+++ calculadora.js
@@ -18,4 +18,36 @@ function multiplicar(a,b){
 };
 
 module.exports = {somar, subtrair, divir ,multiplicar};
+
+
+const {somar, subtrair, dividir, multiplicar } = require('./calculadora');
+
+teste('Deve somar dois numeros corretamente', () => { 
+
+    expect(somar(2,3)).toBe(5);
     
+    expect(somar(-1,1)).toBe(0);
+    
+    expect(somar(0,0)).toBe(0);
+});
+
+
+teste('Deve subtrair dois numeros corretamente', () => {
+    expect(somar(10,5)).toBe(5);
+    
+    expect(somar(0,5)).toBe(-5);
+    
+    expect(somar(-5,-5)).toBe(0);
+});
+
+
+teste('Deve divir dois numeros corretamente', () => {
+    expect(somar(10,5)).toBe(5);
+    
+    expect(somar(-6,2)).toBe(-3);
+});
+
+teste('Deve multiplicar dois numeros corretamente', () => {
+    expect(() => dividir(10,0))
+});
+
